b9536d0a445383a09eb9b421aac3c52103ebb9d1
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2023-05-29 22:24:26 +0200

    test: added test for abuseipdb blocklist

--- molecule/default/tests/test_result.py
+++ molecule/default/tests/test_result.py
@@ -12,3 +12,25 @@ def test_nft_present(host):
     with the requested key types"""
 
     assert host.run("which nft").rc == 0
+
+
+def test_nft_table_firewall(host):
+    """test ssh ssh_known_hosts file has been created,
+    with the requested key types"""
+
+    assert host.run("nft list tables").contains("inet firewall")
+
+
+def nftables_abuseip_service(host):
+    """test that the correct table files are created"""
+
+    assert host.file("/usr/local/bin/manage_nft_abuseip_blocklist.py")
+    assert host.service("update-abuseip-blocklist.timer").is_running
+
+
+def test_table_files_created(host):
+    """test that the correct table files are created"""
+
+    with host.sudo():
+        assert host.file("/etc/nftables/firewall.nft")
+        assert host.file("/etc/nftables/defines.nft")
